839ccb6a4678a605b46dadd5b2950582d8820025
Zeke Sikelianos <zeke@sikelianos.com>
2017-01-23 10:29:51 -0800

    moar profiles

--- scripts/build.js
+++ scripts/build.js
@@ -1,24 +1,25 @@
 const fs = require('fs')
 const path = require('path')
-const owners = require('owners')
 const RateLimiter = require('limiter').RateLimiter
 const limiter = new RateLimiter(5, 'second')
+const lodash = require('lodash')
 const getProfile = require('../lib/get-profile')
-const requireDir = require('require-dir')
-const prefetchedProfiles = requireDir('../profiles')
-const prefetchedUsernames = Object.keys(prefetchedProfiles)
+const allUsernames = require('owners').map(o => o.username)
+const prefetchedUsernames = fs.readdirSync(path.join(__dirname, '../profiles'))
+  .map(filename => path.basename(filename, '.json'))
+const targets = lodash.difference(allUsernames, prefetchedUsernames)
 
-console.log(`owners.length: ${owners.length}`)
-console.log(`prefetchedUsernames.length: ${prefetchedUsernames.length}`)
+console.log(`targets.length: ${targets.length}`)
 
-let usernames = owners
-  .map(o => o.username)
-  .filter(username => !prefetchedUsernames.includes(username))
-
-console.log(`usernames.length: ${usernames.length}`)
-
-usernames.forEach(username => {
+targets.forEach(username => {
   limiter.removeTokens(1, () => {
-    
+    getProfile(username)
+      .catch(error => {
+        if (error.statusCode) {
+          console.error(`${username} failed: (${error.statusCode})`)
+        } else {
+          console.error(error)
+        }
+      })
   })
 })
